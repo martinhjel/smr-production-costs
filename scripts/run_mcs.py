9aed277524080391d74f1b8782e49c2b0194a3b6
= <martin.hjelmeland@ntnu.no>
2024-07-09 15:16:37 +0200

    Update run script

--- scripts/run_mcs.py
+++ scripts/run_mcs.py
@@ -2,9 +2,9 @@ import argparse
 import logging
 from datetime import datetime
 from pathlib import Path
+from smr_mcs.distributions import Uniform
 
 from smr_mcs.config import ScalingOption, SimulationConfig
-from smr_mcs.distributions import Gaussian, Triangular, Uniform
 from smr_mcs.project import load_simulation_projects_from_yaml
 from smr_mcs.simulation_runner import run_simulation, run_simulation_concurrent, store_results
 
@@ -12,7 +12,13 @@ parser = argparse.ArgumentParser(
     prog="SMR Monte Carlo Simulator", description="Perform Monte Carlo simulation for SMR costs."
 )
 
-parser.add_argument("-c", "--concurrent", action="store_true", help="Perform multiprocessing")
+parser.add_argument("-p", "--parallel", action="store_true", help="Perform multiprocessing")
+parser.add_argument(
+    "-s", "--scaling", choices=["manufacturer", "rothwell", "roulstone"], help="SMR Cost scaling method"
+)
+parser.add_argument("-c", "--config", help="path to yaml-file with the config.", default="config/reference.yaml")
+parser.add_argument("-d", "--dataset", help="path to yaml-file with the dataset.", default="data/reference.yaml")
+parser.add_argument("-u", "--unit-doubling", help="How many doublings of each unit production.", default=1)
 
 args = parser.parse_args()
 
@@ -38,26 +44,50 @@ logger.addHandler(console_handler)
 
 
 if __name__ == "__main__":
-    start_time = datetime.now()
-    config = SimulationConfig(
-        n=1000000,
-        electricity_price=Uniform(lower=52.2, upper=95.8),
-        wacc=Uniform(lower=0.04,upper=0.10),
-        scaling=Uniform(lower=0.20, upper=0.75),
-        opt_scaling=ScalingOption.ROTHWELL,
-    )
+    if args.scaling == "manufacturer":
+        opt_scaling = ScalingOption.MANUFACTURER
+    elif args.scaling == "rothwell":
+        opt_scaling = ScalingOption.ROTHWELL
+    elif args.scaling == "roulstone":
+        opt_scaling = ScalingOption.ROULSTONE
 
-    yaml_file = Path.cwd() / "data/reference.yaml"
-    simulation_projects = load_simulation_projects_from_yaml(file_path=yaml_file)
+    logger.info("Scaling type: %s", opt_scaling.value)
+
+    start_time = datetime.now()
+
+    def check_and_get_path(path: str):
+        dataset_file = Path(path)
+        if not dataset_file.exists():
+            dataset_file = Path.cwd() / path
+            if not dataset_file.exists():
+                raise ValueError(f"dataset {path} does not exist")
+        return dataset_file
+
+    dataset_file = check_and_get_path(args.dataset)
+    config_file = check_and_get_path(args.config)
+
+    logger.info("Reading dataset from %s", dataset_file)
+    simulation_projects = load_simulation_projects_from_yaml(file_path=dataset_file)
+
+    logger.info("Reading config from %s", config_file)
+    config = SimulationConfig.from_yaml(yaml_file=config_file)
+    config.opt_scaling = opt_scaling
+    config.unit_doubling = int(args.unit_doubling)
 
     # Execute the simulation
     logger.info("Running simulation")
-    if args.concurrent:
-        num_processes = 3  # Memory restrictive
-        npv_results, lcoe_results = run_simulation_concurrent(simulation_projects, config, num_processes)
+    if args.parallel:
+        num_processes = 2  # Memory restrictive
+        logger.info("Concurrent simulation with %d processes", num_processes)
+        npv_results, lcoe_results, inv_results = run_simulation_concurrent(simulation_projects, config, num_processes)
     else:
-        npv_results, lcoe_results = run_simulation(simulation_projects, config)
+        logger.info("Sequential simulation")
+        npv_results, lcoe_results, inv_results = run_simulation(simulation_projects, config)
+
+    print(lcoe_results.mean())
+    # logger.info("LCOE mean: ", lcoe_results.mean() )
+    # logger.info("LCOE std: ", lcoe_results.std() )
 
     logger.info("Storing results")
-    store_results(npv_results, lcoe_results, config, output_path, simulation_projects)
+    store_results(npv_results, lcoe_results, inv_results, config, output_path, simulation_projects)
     logger.info(f"Finished! Took {datetime.now()-start_time}")
